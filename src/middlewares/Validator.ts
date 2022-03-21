ad6f16bcd15a2e068c73764daa55367abb07ce1f
Ignacio Herrera <ignacioromanherrera@gmail.com>
2022-03-21 15:33:29 -0300

    Chore/Remove Validator unnecesary class

--- src/middlewares/Validator.ts
+++ src/middlewares/Validator.ts
@@ -1,25 +1,7 @@
 import { validate, ValidationError } from "class-validator";
 import { plainToInstance } from "class-transformer";
-// import IMiddleware from "../types/IMiddleware";
 import HttpException from "../exceptions/HttpException";
 import { IRequestHandler } from "../types/IRequestHandler";
-
-// export default class Validator implements IMiddleware {
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   static mount(type: any, skipMissingProperties = false): RequestHandler {
-//     return (req, res, next) => {
-//       validate(plainToInstance(type, req.body), { skipMissingProperties })
-//         .then((errors: ValidationError[]) => {
-//           if (errors.length > 0) {
-//             const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(", ");
-//             next(new HttpException(400, message));
-//           } else {
-//             next();
-//           }
-//         });
-//     };
-//   }
-// }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const myValidator = (type: any, skipMissingProperties = false): IRequestHandler => (req, res, next) => validate(plainToInstance(type, req.body), { skipMissingProperties })
