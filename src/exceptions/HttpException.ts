9e40ceba29c6e80b78301701402e91cb87a43498
Ignacio Herrera <ignacioromanherrera@gmail.com>
2022-04-02 23:33:30 -0300

    Feature/Test suite for exceptions

--- src/exceptions/HttpException.ts
+++ src/exceptions/HttpException.ts
@@ -6,7 +6,7 @@ export default class HttpException extends Error {
 
   public message: string;
 
-  constructor(status: number, message: string) {
+  constructor(status = 500, message = "Something went wrong!") {
     super(message);
     this.status = status;
     this.message = message;
---
+++ src/exceptions/NotFoundException.test.ts
@@ -0,0 +1,13 @@
+import NotFoundException from "./NotFoundException";
+
+describe("NotFoundException", () => {
+  it("Should have 404 status code", () => {
+    const e = new NotFoundException();
+    expect(e.status).toBe(404);
+  });
+
+  it("Should support custom message", () => {
+    const e = new NotFoundException("My message");
+    expect(e.message).toBe("My message");
+  });
+});
