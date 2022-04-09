3d4f44e439dd9f2046261d0bad3d51864865370d
Ignacio Herrera <ignacioromanherrera@gmail.com>
2022-04-08 21:09:10 -0300

    Chore/Update eslint config with "space-before-blocks"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -117,5 +117,6 @@ module.exports = {
 			{ blankLine: "always", prev: ["const", "let", "var"], next: "*" },
 			{ blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"] },
 		],
+		"space-before-blocks": "true",
 	}
 };
